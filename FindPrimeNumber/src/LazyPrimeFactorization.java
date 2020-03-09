9df4c4a6be5ae700abeadd773f5ba4138898b89e
Dung <dungtrinh0702@gmail.coom>
2020-03-09 17:53:03 +0700

    add

---
+++ FindPrimeNumber/src/LazyPrimeFactorization.java
@@ -0,0 +1,22 @@
+public class LazyPrimeFactorization implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (isPrime(i)) {
+                System.out.println("Lazy: " + i);
+            }
+        }
+    }
+
+    public boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ FindPrimeNumber/src/OptimizedPrimeFactorization.java
@@ -0,0 +1,22 @@
+public class OptimizedPrimeFactorization implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (isPrime(i)) {
+                System.out.println("Optimized: " + i);
+            }
+        }
+    }
+
+    public boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
